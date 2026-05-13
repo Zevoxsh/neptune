d47614e1a29349d9a1bb8bd0c5806a4cd684d3bf
JeanClaude Dimitrain <antoine.torres.fr@gmail.com>
2026-05-13 11:21:29 +0200

    feat: serve panel/ static files and redirect / to login

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,10 @@ app.use(cookieParser());
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 app.use('/api', require('./api/index'));
+
+app.use('/panel', express.static(path.join(__dirname, '../panel')));
+app.get('/', (_req, res) => res.redirect('/panel/login.html'));
+
 app.use((_req, res) => res.status(404).json({ error: 'Not found' }));
 
 module.exports = app;
